92f214e90bce4a241b8e62b388592d7fd5ca2090
sammusku <Samyuktha.musku@gmail.com>
2026-02-02 17:21:50 -0600

    actions

--- redis.sh
+++ redis.sh
@@ -33,7 +33,7 @@ VALIDATE $? "enable redis 7"
 dnf install redis -y &>>$LOGS_FILE
 VALIDATE $? "install redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e /protected-mode/ c protected-mode no  /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'  /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connection"
 
 systemctl enable redis &>>$LOGS_FILE
